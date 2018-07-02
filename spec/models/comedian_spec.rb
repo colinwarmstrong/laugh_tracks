57edac4f4eab70446ffb0c424c45225360e5d43f
Colin Armstrong <colinwarmstrong@gmail.com>
2018-07-01 21:55:58 -0600

    Slight refactor to testing structure.

--- spec/models/comedian_spec.rb
+++ spec/models/comedian_spec.rb
@@ -11,30 +11,30 @@ RSpec.describe Comedian do
         expect(comic).to_not be_valid
       end
     end
+  end
 
-    describe 'Class Methods' do
-      it 'self.average_age' do
-        Comedian.create(name: "Hannibal Buress", age: 34)
-        Comedian.create(name: "Paul Mooney", age: 76)
-        Comedian.create(name: "John Mulaney", age: 40)
+  describe 'Class Methods' do
+    it 'self.average_age' do
+      Comedian.create(name: "Hannibal Buress", age: 34)
+      Comedian.create(name: "Paul Mooney", age: 76)
+      Comedian.create(name: "John Mulaney", age: 40)
 
-        expected = Comedian.average_age
+      expected = Comedian.average_age
 
-        expect(expected).to eq(50)
-      end
+      expect(expected).to eq(50)
     end
+  end
 
-    describe 'Instance Methods' do
-      it 'number_of_specials' do
-        comedian = Comedian.create(name: "Tom Segura", age: 38)
-        Special.create(name: "Completely Normal", comedian_id: comedian.id)
-        Special.create(name: "Mostly Stories", comedian_id: comedian.id)
-        Special.create(name: "Something Something", comedian_id: comedian.id)
+  describe 'Instance Methods' do
+    it 'number_of_specials' do
+      comedian = Comedian.create(name: "Tom Segura", age: 38)
+      Special.create(name: "Completely Normal", comedian_id: comedian.id)
+      Special.create(name: "Mostly Stories", comedian_id: comedian.id)
+      Special.create(name: "Something Something", comedian_id: comedian.id)
 
-        expected = comedian.number_of_specials
+      expected = comedian.number_of_specials
 
-        expect(expected).to eq(3)
-      end
+      expect(expected).to eq(3)
     end
   end
 end
